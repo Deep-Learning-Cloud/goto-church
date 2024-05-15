326450e5bc78ef62f033fab8f2e969a556158c35
Ellie <hi@ellie.moe>
2024-05-15 18:28:27 +0200

    Add header (#8)
    * Add header
    
    * Fix bugs
    
    * Fix more bugs

---
+++ src/components/header.tsx
@@ -0,0 +1,17 @@
+import { component$ } from "@builder.io/qwik";
+import logo from "./logo-text.svg";
+import { Link } from "@builder.io/qwik-city";
+
+export const Header = component$(() => {
+  return (
+    <header class="flex min-h-[90px] w-full flex-wrap items-center justify-between gap-10 rounded-[50px] bg-white px-10 py-5 text-[#875E90] shadow-xl">
+      <img src={logo} alt="logo" width="260" height="50" />
+      <div class="flex flex-wrap items-center justify-center gap-10 font-sans text-2xl font-bold">
+        <Link href="/">Home</Link>
+        <Link href="/mission">Mission</Link>
+        <Link href="/donate">Donate</Link>
+        <Link href="/disclosure">Disclosure</Link>
+      </div>
+    </header>
+  );
+});
--- src/routes/results/[id]/index.tsx
+++ src/routes/results/[id]/index.tsx
@@ -6,6 +6,7 @@ import type { GenerateRequest } from "~/utils/store";
 import { getGenerateRequest, saveGenerateRequest } from "~/utils/store";
 import "./loader.css";
 import { nanoid } from "nanoid";
+import { Header } from "~/components/header";
 
 // We have a new support group for people with mental health issues, meetings on Saturdays at 1 pm
 
@@ -67,36 +68,21 @@ export default component$(() => {
 
   if (!results.value) {
     return (
-      <div class="flex w-full flex-col items-center gap-8 p-12 font-serif text-xl font-bold text-purple">
-        Getting your quotes, hang on a moment...
-        <span class="loader" />
+      <div class="flex flex-col items-center gap-16 p-16">
+        <Header />
+        <div class="flex w-full flex-col items-center gap-8 p-12 font-serif text-xl font-bold text-purple">
+          Getting your quotes, hang on a moment...
+          <span class="loader" />
+        </div>
       </div>
     );
   }
 
   return (
     <div class="flex flex-col items-center gap-16 p-16">
+      <Header />
       <div class="flex w-full max-w-3xl flex-col items-center gap-6 text-center font-serif font-bold text-purple">
         <h1 class="text-5xl leading-snug">Here are your quotes</h1>
-        <div class="max-w-xl text-xl">
-          <Link href="/">
-            <Button>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="currentColor"
-                class="h-6 w-6"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M11.03 3.97a.75.75 0 0 1 0 1.06l-6.22 6.22H21a.75.75 0 0 1 0 1.5H4.81l6.22 6.22a.75.75 0 1 1-1.06 1.06l-7.5-7.5a.75.75 0 0 1 0-1.06l7.5-7.5a.75.75 0 0 1 1.06 0Z"
-                  clip-rule="evenodd"
-                />
-              </svg>
-              Go back
-            </Button>
-          </Link>
-        </div>
         {generateRequest.value?.input?.type === "input" && (
           <p class="text-center font-sans text-xl text-purple">
             What's happening: {generateRequest.value.input.value}

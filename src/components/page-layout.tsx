63ca8e550901afecc4da24c499a6a3570b04e80d
Ellie Fagerberg <hi@ellie.moe>
2024-05-22 11:32:01 +0200

    Decrease padding and gap on mobile

--- src/components/page-layout.tsx
+++ src/components/page-layout.tsx
@@ -3,7 +3,7 @@ import { Header } from "./header";
 
 export const PageLayout = component$(() => {
   return (
-    <div class="flex flex-col items-center gap-16 p-16">
+    <div class="flex flex-col items-center gap-10 p-6 md:gap-16 md:p-16">
       <Header />
 
       <div class="flex w-full max-w-3xl flex-col items-center gap-6 text-center font-serif font-bold text-purple">
